a363a4a7f5179707d777dc348ea2cb050ea7300f
GitGeddes <hellohead102@gmail.com>
2026-06-19 07:57:50 -0700

    Fix table location
    - Add some comments

--- client/src/views/MoviesView.vue
+++ client/src/views/MoviesView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getAllMovies, getMoviesByPage, type Movie } from '@/services/api'
 import { ref, type Ref } from 'vue'
+import { VCard } from 'vuetify/components'
 
 const search = ref('')
 const headers = [
@@ -17,9 +18,12 @@ const movies: Ref<Movie[]> = ref([])
 const page = ref(1)
 const itemsPerPage = ref(10)
 
+// Show the loading text in the data table.
+const isLoading = ref(true)
+
 // Initialize movies table on load
 getMovies()
-
+// Get all of the movies and update the data table.
 function getMovies() {
   getAllMovies().then((res) => {
     if (!res) {
@@ -27,9 +31,12 @@ function getMovies() {
       return
     }
     movies.value = res
+    // Reset the loading state
+    isLoading.value = false
   })
 }
 
+// Search the data table by the title.
 function searchFilter(value: string | null, query: string | null) {
   return (
     value != null &&
@@ -39,10 +46,12 @@ function searchFilter(value: string | null, query: string | null) {
   )
 }
 
+// Get the next page of movies.
 function onClickGetPage() {
   getPage(page.value + 1)
 }
 
+// Get the current or a given page of movies and update the page number.
 function getPage(pageNum: number = page.value) {
   getMoviesByPage(pageNum).then((res) => {
     if (!res) {
@@ -56,35 +65,56 @@ function getPage(pageNum: number = page.value) {
 </script>
 
 <template>
-  <v-btn @click="onClickGetPage">Get Next Page</v-btn>
+  <main>
+    <v-btn @click="onClickGetPage">Get Next Page</v-btn>
 
-  <v-card title="Movies" flat>
-    <v-data-table
-      :headers="headers"
-      :items="movies"
-      :search="search"
-      :custom-filter="searchFilter"
-      :items-per-page="itemsPerPage"
-      sort-asc-icon="mdi-sort-ascending"
-      sort-desc-icon="mdi-sort-descending"
-      sort-icon="mdi-swap-vertical"
-    >
-      <template v-slot:top>
-        <v-text-field
-          v-model="search"
-          label="Search"
-          prepend-inner-icon="mdi-magnify"
-          variant="outlined"
-          hide-details
-          single-line
-        ></v-text-field>
-      </template>
+    <v-card title="Movies" flat>
+      <v-data-table
+        :headers="headers"
+        :items="movies"
+        :search="search"
+        :custom-filter="searchFilter"
+        :items-per-page="itemsPerPage"
+        :loading="isLoading"
+        loading-text="Loading Movies"
+        sort-asc-icon="mdi-sort-ascending"
+        sort-desc-icon="mdi-sort-descending"
+        sort-icon="mdi-swap-vertical"
+      >
+        <template v-slot:top>
+          <v-text-field
+            v-model="search"
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+            hide-details
+            single-line
+          ></v-text-field>
+        </template>
 
-      <template v-slot:item.poster="{ item }">
-        <v-card class="my-2" elevation="1" rounded>
-          <v-img :src="`${item.poster}`" height="64" cover></v-img>
-        </v-card>
-      </template>
-    </v-data-table>
-  </v-card>
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.poster="{ item }">
+          <v-card class="my-2" elevation="1" rounded>
+            <v-img :src="`${item.poster}`" height="64" cover></v-img>
+          </v-card>
+        </template>
+
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.title="{ item }">
+          <v-card flat>
+            <v-card-text class="text-truncate" style="width: 300px">{{ item.title }}</v-card-text>
+          </v-card>
+        </template>
+
+        <!-- eslint-disable-next-line vue/valid-v-slot -->
+        <template v-slot:item.genres="{ item }">
+          <v-card class="max-h-[64px]" flat>
+            <v-card-text class="text-truncate" style="max-width: 200px">{{
+              item.genres
+            }}</v-card-text>
+          </v-card>
+        </template>
+      </v-data-table>
+    </v-card>
+  </main>
 </template>
