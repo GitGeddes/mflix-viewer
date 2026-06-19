23845e0d41a1dabf8a7e58ce345628aa06bf7c39
GitGeddes <hellohead102@gmail.com>
2026-06-18 21:07:59 -0700

    Enable CORS on server

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,17 @@
+// Load env variables
 import "./src/loadEnvironment.ts";
 
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import indexRouter from './routes/index.ts';
 import usersRouter from './routes/users.ts';
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
