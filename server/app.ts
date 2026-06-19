d5b8f2f0162e6e2d33c9df25fb419b9092afa29f
GitGeddes <hellohead102@gmail.com>
2026-06-18 20:16:55 -0700

    Convert server to module and TypeScript

---
+++ server/app.ts
@@ -0,0 +1,22 @@
+import "./src/loadEnvironment.ts";
+
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index.ts';
+import usersRouter from './routes/users.ts';
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('./public'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
